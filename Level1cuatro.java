65f7d7220a6c076eff86ebb06ae5f79e20bddca0
Katherine <fernandezkatherine8@gmail.com>
2021-10-14 20:14:49 -0300

    agregar ejer4

--- Level1cuatro.java
+++ Level1cuatro.java
@@ -2,7 +2,7 @@ package JavaInformatorio2021;
 
 public class Level1cuatro {
      public static void main(String[] args) {
-        System.out.println("HOLA MUNDO");
+        System.out.println("HOLA     MUNDO");
     }
     
 }
